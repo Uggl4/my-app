833e8d2ef9014ad0f328e24a7da1e8d1423f5e8e
uggla <eliascastillopro2001@gmail.com>
2022-06-16 12:35:23 -0400

    Entrar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Principal } from "./Principal";
+
+import "./styles.css";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+  const database = [
+    {
+      username: "admin",
+      password: "admin"
+    },
+    {
+      username: "admin1",
+      password: "admin1"
+    }
+  ];
+
+  const errors = {
+    uname: "Usuario invalido",
+    pass: "Contraseña invalida"
+  };
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // contraseña invalida
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Usuario invalido
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Genera mensaje error
+
+  
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // Inicio de sesion
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Usuario </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Contraseña </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+         <input type="submit" />
+        </div>
+      </form>
     </div>
+  );
+    const a = ()=> {
+      setIsSubmitted(false);
+      console.log(isSubmitted);
+      render(<App/>);
+    }
+  return (
+  <><Principal />
+  <button onClick={a}>Cerrar sesion</button>
+  </>
+  
   );
 }
 
---
+++ src/Principal.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Principal = () => {
+  return (
+    <>Principal
+    
+    </>
+    
+  )
+}
